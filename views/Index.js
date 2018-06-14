6fb510f42e491da5a5569309b4aab266124c1332
chenjiayao <598423644@qq.com>
2018-06-14 11:37:35 +0800

    范团RN    忽略ios/Pods、ios/bundle文件夹    chenjiaguang

---
+++ views/Index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+    ScrollView,
+    TouchableOpacity,
+    View,
+    Image,
+    Text,
+    StyleSheet
+} from 'react-native';
+import Iconfont from "../components/cxicon/CXIcon";
+import px2dp from '../lib/px2dp'
+import { ifIphoneX } from 'react-native-iphone-x-helper'
+const bg = require('../static/image/apply_banner.png')
+import Button from 'apsl-react-native-button' // 第三方button库，RN官方的库会根据平台不同区别，这里统一
+// 申请头条首页
+
+export default class HeadlineIndex extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {}
+    }
+    static navigationOptions = {
+        title: ''
+    };
+    componentWillMount () {
+        console.log('componentWillMount', this.props)
+        this.props.navigation.replace(this.props.screenProps.route)
+    }
+    render() {
+        return <View style={{flex: 1}}></View>
+    }
+}
