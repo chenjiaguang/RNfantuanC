ed6ef6e41123f06ded1c2bd319fc8bdea3b839cb
hyf <459909293@qq.com>
2018-06-27 16:32:17 +0800

    原声跳转

--- views/ActivityDetail.js
+++ views/ActivityDetail.js
@@ -6,7 +6,8 @@ import {
     Image,
     StyleSheet,
     Linking,
-    Animated
+    Animated,
+    TouchableWithoutFeedback
 } from 'react-native';
 import px2dp from '../lib/px2dp'
 import { ifIphoneX, getStatusBarHeight } from 'react-native-iphone-x-helper'
@@ -16,6 +17,7 @@ import CodeInput from '../components/CodeInput' // 自己封装的获取验证�
 import Button from 'apsl-react-native-button' // 第三方button库，RN官方的库会根据平台不同区别，这里统一
 import Toast from  '../components/Toast'
 import commonStyle from "../static/commonStyle";
+import GoNativeModule from '../modules/GoNativeModule'
 
 export default class ActivityDetail extends React.Component {  // 什么参数都不传，则默认是绑定手机都页面，传入isRebind为true时表示新绑手机，界面稍有差异
     constructor (props) {
@@ -54,6 +56,18 @@ export default class ActivityDetail extends React.Component {  // 什么参数�
                 position: 'absolute'
             }
         }
+    }
+    onJumpPublishArticleDynamic=(id,name,actid)=>{
+        GoNativeModule&&GoNativeModule.goPublishArticleDynamic("4","测试测试测试","10")
+    }
+    onJumpPublishDynamic=(id,name,actid)=>{
+        GoNativeModule&&GoNativeModule.goPublishDynamic("4","测试测试测试","10")
+    }
+    onJumpActivityMap=(destName,latitude,longtitude)=>{
+        GoNativeModule&&GoNativeModule.goActivityMap("cccccccc","110.309207","20.025535")
+    }
+    onJumpActivityShow=(id,name)=>{
+        GoNativeModule&&GoNativeModule.goActivityShow("10","测试测试测试")
     }
     handleScroll = (event) => {
         this.props.navigation.setParams({'opacity': event.nativeEvent.contentOffset.y >= 0 ? event.nativeEvent.contentOffset.y / (px2dp(332) - getStatusBarHeight(true)) : 0})
@@ -120,6 +134,19 @@ export default class ActivityDetail extends React.Component {  // 什么参数�
         let {initialHeight, currentHeight, maxHeight, animationHeight} = this.state
         return <ScrollView style={styles.scrollView} onScroll={this.handleScroll} scrollEventThrottle={15}>
             <Image source={{uri: bannerUrl}} style={styles.header} />
+            
+            <TouchableWithoutFeedback onPress={() => this.onJumpPublishArticleDynamic(0,0,0)}>
+            <Text>跳转晒长文</Text>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={() => this.onJumpPublishDynamic(0,0,0)}>
+            <Text>跳转晒动态</Text>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={() => this.onJumpActivityMap(0,0,0)}>
+            <Text>跳转地图</Text>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={() => this.onJumpActivityShow(0,0,0)}>
+            <Text>跳转大家都在晒</Text>
+            </TouchableWithoutFeedback>
             <View style={styles.contentWrapper}>
                 <Text style={styles.title}>{title}</Text>
                 <Text style={styles.from}>来自"{from}"的活动</Text>
