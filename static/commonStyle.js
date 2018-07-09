22666579c279d19e6dd34199dbc3336a8191f34f
hyf <459909293@qq.com>
2018-07-09 09:09:16 +0800

    actionbar适配

--- static/commonStyle.js
+++ static/commonStyle.js
@@ -1,5 +1,7 @@
 import px2dp from "../lib/px2dp";
 import { Platform } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+let statusBarHeight = getStatusBarHeight(true)
 const commonStyle = {
   /** color **/
   // 常用颜色
@@ -44,20 +46,32 @@ const commonStyle = {
     left: px2dp(18), // 导航左侧距离
     right: px2dp(18) // 导航右侧距离
   },
-  headerTitleStyle: Platform.OS == 'android' ? {
-    fontSize: 19,//不用px2dp
-    color: '#333',
-    alignSelf: 'center',
-    textAlign: 'center',
-    flex: 1,
-    fontWeight: 'normal'
-  } : {
+  headerTitleStyle: Platform.OS == 'android' ?
+    {
+      fontSize: 19,//不用px2dp
+      color: '#333',
+      alignSelf: 'center',
+      textAlign: 'center',
+      flex: 1,
+      fontWeight: 'normal'
+    } :
+    {
       fontSize: px2dp(34),
       color: '#333',
       alignSelf: 'center',
       textAlign: 'center',
       flex: 1
     },
+    headerStyleNormal: {
+      width: px2dp(750),
+      height: Platform.OS === 'android' ? 50 + statusBarHeight : px2dp(90),
+      paddingTop: 0,
+      backgroundColor: '#fafafa',
+      borderTopColor: Platform.OS === 'android' && Platform.Version < 23 ? '#8c8c8c' : '#fafafa',//8c=fa*(256-112)/256
+      borderTopWidth: Platform.OS === 'android' ? statusBarHeight : 0,
+      borderBottomWidth: 0,
+      elevation: 0,
+  }
 }
 
 export default commonStyle
