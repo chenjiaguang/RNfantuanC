d47112c6755066232e33137497d3fef3f160ea2b
hyf <459909293@qq.com>
2018-09-10 16:44:28 +0800

    hyf 活动详情的标题淡入淡出效果

--- components/HeadNav.js
+++ components/HeadNav.js
@@ -24,16 +24,16 @@ class HeadNav extends Component {
         return 'rgb(' + color + ',' + color + ',' + color + ')';
     }
     render() {
-        let {fetching} = this.props
+        let { fetching } = this.props
         let rgb = this.getRGB()
-        let text = this.state.value.toString() === '1' ? this.props.title : ''
-        return <View style={[styles.container, { backgroundColor: fetching ? 'rgba(250,250,250,1)' : 'rgba(250,250,250,' + this.state.value + ')', borderBottomWidth: px2dp(1), borderBottomColor:  (this.state.value.toString() === '1' || fetching) ? '#E5E5E5' : 'transparent'}]}>
+        let text = this.props.title
+        return <View style={[styles.container, { backgroundColor: fetching ? 'rgba(250,250,250,1)' : 'rgba(250,250,250,' + this.state.value + ')', borderBottomWidth: px2dp(1), borderBottomColor: (this.state.value.toString() === '1' || fetching) ? '#E5E5E5' : 'transparent' }]}>
             <TouchableWithoutFeedback disabled={false} onPress={() => { this.props.navigation.pop() }}>
                 <View style={{ width: px2dp(80), height: px2dp(90), flexDirection: 'row', alignItems: 'center' }}>
                     <Iconfont name='go_back' size={px2dp(38)} color={fetching ? '#666' : rgb} style={{ paddingLeft: px2dp(18) }} />
                 </View>
             </TouchableWithoutFeedback>
-            <Text style={commonStyle.headerTitleStyle}>{text}</Text>
+            <Text style={[commonStyle.headerTitleStyle, { color: fetching ? 'rgba(0,0,0,0)' : 'rgba(0,0,0,' + this.state.value + ')' }]}>{text}</Text>
             {this.props.headerRight(fetching ? '#666' : rgb)}
 
         </View >;
